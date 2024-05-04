19acdc94e58c6eda71a30c536f34d487090c2746
Elragency <elragency@gmail.com>
2024-05-04 12:29:43 -0400

    first commit

---
+++ src/utils/navigation.ts
@@ -0,0 +1,42 @@
+
+const navBarLinks = [
+  { name: "Accueil", url: "/" },
+  { name: "Produits", url: "/products" },
+  { name: "Services", url: "/services" },
+  { name: "Blog", url: "/blog" },
+  { name: "Contact", url: "/contact" },
+];
+
+const footerLinks = [
+  {
+    section: "Écosystème",
+    links: [
+      { name: "Documentation", url: "/fr/welcome-to-docs/" },
+      { name: "Outils et Équipements", url: "/fr/products" },
+      { name: "Services de Construction", url: "/fr/services" },
+    ],
+  },
+  {
+    section: "Société",
+    links: [
+      { name: "À propos de nous", url: "#" },
+      { name: "Blog", url: "/fr/blog" },
+      { name: "Carrières", url: "#" },
+      { name: "Clients", url: "#" },
+    ],
+  },
+];
+
+const socialLinks = {
+  facebook: "#",
+  x: "#",
+  github: "https://elragency.com",
+  google: "#",
+  slack: "#",
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
